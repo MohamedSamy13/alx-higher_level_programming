9101f12ff8bf1450c6a0e3a645e9edae34873bb4
MohamedSamy13 <mohammed7samy777@gmail.com>
2024-03-02 11:13:58 -0800

    546

---
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" This scripts GETS a header variable """
+import sys
+import urllib.request
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    with urllib.request.urlopen(url) as response:
+        value = response.headers.get('X-Request-Id')
+        print(value)
