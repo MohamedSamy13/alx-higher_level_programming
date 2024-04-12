835a307b628c098dfa4f37c0dd0299ab1fdb77f6
MohamedSamy13 <mohammed7samy777@gmail.com>
2024-04-12 07:50:50 -0700

    dkd

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,22 +1,3 @@
 #!/usr/bin/python3
-for i in range(122, 96, -1):
-    if i % 2 != 0:
-        i = i - 32
-    print("{}".format(chr(i)), end="")
-
-
-
-
-
-vi 101-remove_char_at.py
-
-#!/usr/bin/python3
-def remove_char_at(str, n):
-    new = ""
-    i = 0
-    for c in str:
-        if i != n:
-            new += c
-        i += 1
-    return new
-
+for ch in reversed(range(97, 123)):
+    print("{:c}".format(ch if (ch % 2 == 0) else (ch - 32)), end='')
